3b5e05e23f3ab81b3171b2ec5ab65a2c3a3e9d8d
Steven Hollidge <stevenhollidge@hotmail.com>
2014-11-15 11:06:35 +0100

    initial commit

---
+++ Benchmark/scripts/directives/igDctv.js
@@ -0,0 +1,50 @@
+﻿angular.module('app')
+.directive('igGrid', function () {
+    return {
+        restrict: 'E',
+        replace: true,
+        scope: {
+            options: '='
+        },
+        template: '<table></table>',
+        link: function (scope, element, attrs) {
+            if (scope.options) {
+                $(element).igGrid(scope.options);
+            }
+        }
+    }
+})
+
+.directive('igChart', function () {
+    return {
+        restrict: 'E',
+        replace: true,
+        scope: { data: '=', binding: '@' },
+        template: '<div></div>',
+        link: function (scope, element, attrs) {
+            if (scope.data && scope.binding) {
+                var bindingY = scope.binding.split(',')[1];
+                $(element).igDataChart({
+                    width: '100%',
+                    height: '450px',
+                    dataSource: scope.data,
+                    axes: [
+                        { name: 'XAxis', type: 'categoryX' },
+                        { name: 'YAxis', type: 'numericY' }
+                    ],
+                    series: [
+                        {
+                            name: bindingY,
+                            valueMemberPath: bindingY,
+                            xAxis: 'XAxis',
+                            yAxis: 'YAxis',
+                            type: 'line',
+                            isTransitionInEnabled: false
+                        }
+                    ]
+                });
+            }
+        }
+    }
+});
+
